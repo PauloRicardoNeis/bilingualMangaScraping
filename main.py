c03e4ad6bfc5a566a983d5bb1f5148cf23157c80
Normie <paulonormie@gmail.com>
2023-12-07 02:27:01 -0300

    It parses dialog bubbles properly now

--- main.py
+++ main.py
@@ -1,54 +1,32 @@
+import asyncio
+import aiohttp
+
+
 from requests_html import AsyncHTMLSession
 from requests_html import HTMLSession
 import asyncio
 import time
-asession = AsyncHTMLSession()
-
 
 def get_manga_page(n):
-    url_string = "https://bilingualmanga.org/manga/636f632fbd9f0ab40a280919?lang=jp&chen=0&chjp=0&enp=0&jpp={}#img_store".format(n)
+    url_string = "https://bilingualmanga.org/manga/6408ace1df8a4333a0c37e7a?lang=jp&chen=0&chjp=0&enp=0&jpp={}#img_store".format(n)
 
 
     r = s.get(url_string)
-    r.html.render(sleep=2, timeout=10000)
+    r.html.render(sleep=3, timeout=10000)
 
-    element = r.html.find('#img_store', first=True)
-    print(n+1)
-    if(element.text != None):
-        return element.text
-    return 'empty'
+    element = r.html.find('.ocrtext')
+    print(n, end='\n\n')
+    if(element is not None):
+        return element
+    return 'whaaat'
 
 def main(limit):
+    l = list()
     for n in range(limit):
-        print(get_manga_page(n))
+        l = get_manga_page(n)
+        for i in l:
+            print(i.text)
+            print()
 
 s = HTMLSession()
 main(205)
-
-
-# for n in range(205):
-#     url_string = "https://bilingualmanga.org/manga/636f632fbd9f0ab40a280919?lang=jp&chen=0&chjp=0&enp=0&jpp={}#img_store".format(n)
-#
-#     session = HTMLSession()
-#     response = session.get(url_string)
-#     response.html.render(sleep=2, timeout=10000)
-#
-#     element = response.html.find('#img_store', first=True)
-#     print(n+1)
-#     if(element.text != None):
-#         print(element.text)
-#
-
-# from requests_html import AsyncHTMLSession
-# asession = AsyncHTMLSession()
-#
-# async def get_pythonorg():
-#     r = await asession.get('https://python.org/')
-#
-# async def get_reddit():
-#     r = await asession.get('https://reddit.com/')
-#
-# async def get_google():
-#    r = await asession.get('https://google.com/')
-#
-# session.run(get_pythonorg, get_reddit, get_google)
